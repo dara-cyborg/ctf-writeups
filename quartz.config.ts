a24b15b736564cd425e8fad760f84a52290243da
Mongkol <daramongkol.chan@gmail.com>
2026-05-27 00:32:16 +0700

    fix: align config baseUrl to target custom domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Dara's Sandbox 🏴‍☠️",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ctf-writeups-dara-crawler.xyz", // <--- Fixed to your domain
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
